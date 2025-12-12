41b58f8d25792945b03bd343c441de936441cc9b
Prashanth662 <120783560+Prashanth662@users.noreply.github.com>
2025-12-12 11:32:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const chatbotToggler=document.querySelector("#chatbot-toggler");
 const closeChatbot=document.querySelector("#close-chatbot");
 
 
-const API_KEY="AIzaSyDPo84zbl0A6z_adCTPi_sQAQYDqtD63oE";
+const API_KEY="AIzaSyCoO_k7U6gvVGJ_UHdxD0y8FcI7QBrl1uI";
 const API_URL= `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData={
